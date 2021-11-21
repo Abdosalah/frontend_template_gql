8c6528dab0d5b26cc017bee6a6c19cc7054f0235
Abdo <abdosalah3105@gmail.com>
2021-11-21 17:07:06 +1100

    Add apollo and use first query

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
-import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+
 import Home from './Pages/Home'
 
 const About = () => <h1>ABOUTTTTTT!!!!!!!</h1>
 
+const client = new ApolloClient({
+	uri: 'http://localhost:3030/graphql',
+	cache: new InMemoryCache(),
+})
+
 const App = () => {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/about" element={<About />} />
-			</Routes>
-		</BrowserRouter>
+		<ApolloProvider client={client}>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+				</Routes>
+			</BrowserRouter>
+		</ApolloProvider>
 	)
 }
 
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import { gql, useQuery } from '@apollo/client'
+
+const QUERY = gql`
+	query {
+		health
+	}
+`
 
 const Home = () => {
-  return (
-    <div>
-      HOME PAGE HERER!!!
-    </div>
-  )
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const { loading, error: _, data } = useQuery<{ health: boolean }>(QUERY)
+
+	if (loading === true) {
+		return <h1>LOADING!!!</h1>
+	}
+
+	return <div>HOME PAGE HERER!!! {data?.health ? 'true' : 'false'}</div>
 }
 
 export default Home
